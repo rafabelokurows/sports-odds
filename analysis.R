97bb93959ceef39401bb8891822fb482410cdb84
Rafael Belokurows <rafabelokurows@gmail.com>
2023-06-19 08:44:39 +0100

    Add files via upload

--- analysis.R
+++ analysis.R
@@ -1,7 +1,8 @@
 library(tidyverse)
-setwd("C:\\Users\\belokurowsr\\OneDrive - Kantar\\Desktop\\baseball-ods")
+setwd("C:\\Users\\rafae\\OneDrive\\Área de Trabalho\\baseball-odds\\data")
 
-files = list.files() %>% file.info() %>% rownames()
+
+files = list.files(pattern = "baseballgames") %>% file.info() %>% rownames()
 
 df = data.frame()
 for (file in files[20:25]){
@@ -9,31 +10,33 @@ for (file in files[20:25]){
   df = bind_rows(df,aux)
 }
 df
-teams = readRDS("C:\\Users\\belokurowsr\\OneDrive - Kantar\\Desktop\\R pessoal\\mlb-winners\\data\\work\\teams.rds")
 
-df %>% head() %>% filter( grepl(name_x,teams$club_name))
+#teams = readRDS("C:\\Users\\belokurowsr\\OneDrive - Kantar\\Desktop\\R pessoal\\mlb-winners\\data\\work\\teams.rds")
+teams = readRDS("C:\\Users\\rafae\\OneDrive\\Área de Trabalho\\baseball-odds\\data\\work\\teams.rds")
 
 df= df  %>% #head(100) %>%
   filter(grepl(paste(teams$club_name, collapse = "|"), name_x) & !str_detect(name_x,"KIA|LG|Lotte|KT|Hiroshima|Tokyo|Yokohama|Chunichi|Yomiuri|Hanshin")) %>%
   mutate(day = as.Date(str_extract(filename, "\\d{8}"), format = "%Y%m%d"),
          hour = sub(".*([0-9]{4})\\.csv$", "\\1", filename))
 
-res = readRDS("C:\\Users\\belokurowsr\\OneDrive - Kantar\\Desktop\\R pessoal\\mlb-winners\\data\\work\\df2.rds")
-res %>% View()
-res2 = res %>% filter(officialDate %in% c("2023-04-14", "2023-04-16", "2023-04-17")) %>%
+res = readRDS("C:\\Users\\rafae\\OneDrive\\Área de Trabalho\\baseball-odds\\data\\work\\df2.rds")
+
+res2 = res %>% #filter(officialDate %in% c("2023-04-14", "2023-04-16", "2023-04-17")) %>%
   select(game_pk,officialDate,teams.home.team.name,teams.away.team.name,teams.home.score,teams.away.score,teams.home.isWinner,
-         teams.home.leagueRecord.pct,teams.away.leagueRecord.pct) %>% filter(officialDate == "2023-04-17") %>%
+         teams.home.leagueRecord.pct,teams.away.leagueRecord.pct) %>% 
+  filter(officialDate == "2023-04-17") %>%
   left_join(teams %>% select(team_full_name,home = club_name),by=c("teams.home.team.name"="team_full_name")) %>%
   left_join(teams %>% select(team_full_name,away = club_name),by=c("teams.away.team.name"="team_full_name"))
 
 
-df %>% filter(day == "2023-04-16" & isLive_x=="False") %>% #count(id_y,name_x)%>%
+df %>% filter(day == "2023-04-17" & isLive_x=="False") %>% #count(id_y,name_x)%>%
   separate(name_x, c("home", "away"), " - ")  %>%
   mutate( away = gsub("Cleveland ","",away),
           home = gsub("Cleveland ","",home)) %>%
   inner_join(res2) %>% arrange(game_pk)
 
-df %>% filter(day == "2023-04-17" & isLive_x=="False") %>%
+games = df %>% 
+  filter(day == "2023-04-17" & isLive_x=="False") %>%
   group_by(id_y,name_x) %>%
   #filter(name_x =="Red Sox - Angels") %>% #count(id_y,name_x)%>%
   separate(name_x, c("home", "away"), " - ") %>%
@@ -55,10 +58,81 @@ df %>% filter(day == "2023-04-17" & isLive_x=="False") %>%
                           teams.home.leagueRecord.pct<teams.away.leagueRecord.pct~"away",
                           TRUE~"tie"),
   best_record_won = if_else(best_record=="home"& teams.home.isWinner |
-                              best_record == "away"& !teams.home.isWinner,TRUE,FALSE)) %>% openxlsx::write.xlsx("teste.xlsx")
-  clipr::write_clip()
+                              best_record == "away"& !teams.home.isWinner,TRUE,FALSE)) %>% 
+  mutate(diff_record = if_else(teams.home.leagueRecord.pct>teams.away.leagueRecord.pct,
+                               teams.home.leagueRecord.pct-teams.away.leagueRecord.pct,teams.away.leagueRecord.pct-teams.home.leagueRecord.pct))
+games %>%
+  ungroup %>% 
+  summarize(mean(home_odds),
+            mean(away_odds),
+            mean(winner_odds),
+            mean(winner_margin),
+            mean(favorite_won),
+            mean(best_record_won),
+            mean(teams.home.isWinner),
+            mean(diff_record))
+
+games %>% group_by() %>% 
+  
+  
+# %>% 
+#   openxlsx::write.xlsx("teste3.xlsx")
+
+multiples = games %>% 
+  ungroup %>% 
+  mutate(units = n(),
+         units_triple = units/ 4,
+         units_double = units/6,
+         double = ceiling(row_number() / 2),
+         triple = ceiling(row_number() / 3)) 
+
+triples = multiples %>%
+  group_by(triple) %>% 
+  mutate(games=n(),won=sum(best_record_won)) %>%
+  mutate(hit_not = if_else(won==games,"hit","not")) %>% 
+  group_by(triple) %>% 
+  mutate(total_winner=prod(winner_odds)) %>% 
+  mutate(total_winner = ifelse(hit_not=="hit",total_winner*units_triple,0)) %>% 
+  select(winner_odds,tail(colnames(.))) %>% 
+  summarize(total_money=sum(unique(total_winner)))  %>% 
+  mutate(triple = paste0("triple ",triple)) %>% 
+  rename(name=triple,value=total_money)
 
 
+doubles = multiples %>%
+  group_by(double) %>% 
+  mutate(games=n(),won=sum(best_record_won)) %>%
+  mutate(hit_not = if_else(won==games,"hit","not")) %>% 
+  group_by(double) %>% 
+  mutate(total_winner=prod(winner_odds)) %>% 
+  mutate(total_winner = ifelse(hit_not=="hit",total_winner*units_double,0)) %>% 
+  select(winner_odds,tail(colnames(.))) %>% 
+  summarize(total_money=sum(unique(total_winner))) %>% 
+  mutate(double = paste0("double ",double)) %>% 
+  rename(name=double,value=total_money)
 
 
-res %>% filter(officialDate == "2023-04-16")
+games %>% 
+  ungroup %>% summarize(
+    units = n(),
+    units_triple = ceiling(units/ 3),
+    units_double = ceiling(units/2),
+    bet_home = sum(home_odds[games$teams.home.isWinner==T])-units,
+    bet_away = sum(away_odds[games$teams.home.isWinner==F])-units,
+    bet_favorite = sum(winner_odds[games$favorite_won==T])-units,
+    bet_underdog = sum(winner_odds[games$favorite_won==F])-units,
+    bet_underdog = sum(winner_odds[games$best_record_won==T])-units
+  ) %>% pivot_longer(everything()) %>% 
+  bind_rows(triples) %>% 
+  bind_rows(
+    triples %>% 
+      summarize(value=sum(value)) %>% 
+      mutate(name="total triple")
+  ) %>% 
+  bind_rows(doubles)%>% 
+  bind_rows(
+    doubles %>% 
+      summarize(value=sum(value)) %>% 
+      mutate(name="total double")
+  ) %>%
+  mutate(date="2023-04-16") 
